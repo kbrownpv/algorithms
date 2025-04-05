0e9df27c2993feb0c0ee24137adaa9f44493c1ed
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-04-04 20:27:57 -0500

    Update postOrderTraversal.cpp

--- postOrderTraversal.cpp
+++ postOrderTraversal.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val) : data(val), left(nullptr), right(nullptr) { }
+};
+
+void postOrderTraversal(Node* root) {
+    if (root == nullptr)
+        return;
+    
+    postOrderTraversal(root->left);   
+    postOrderTraversal(root->right);  
+    cout << root->data << " ";        
+}
+
+int main() {
+  
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    
+    cout << "Post-order Traversal: ";
+    postOrderTraversal(root);
+    cout << endl;
+    
+    return 0;
+}
 
